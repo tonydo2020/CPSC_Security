d61be89c05f9346737cc238c7d164837e497edef
tonydo2020 <55472788+tonydo2020@users.noreply.github.com>
2019-11-09 10:22:01 -0800

    updated pls.py

--- pls.py
+++ pls.py
@@ -49,7 +49,49 @@ def getEncrypt(keylist, string):
     pkey_count = 0
     c = ""
 
-    for a in len(string):
+    for a in range(len(keylist)):
+        c = keylist[a]
+        if c == '(':
+            pkey = keylist[a + 1]
+            break
+
+    for b in range(len(keylist)): # starting at the next key value after the (
+        if keylist[b] == pkey:
+            pkey_count = pkey_count + 1
+
+    encrypt_string(pkey, pkey_count, keylist, string)
+
+
+def encrypt_string(pkey, pkey_count, keylist, string):
+
+    if pkey == '!':
+        outer_list = []
+
+        b = 0
+        for a in range(pkey_count):
+            if keylist[a] == '!':
+                inner_list = []
+                while string[b] != "":
+                    if ord(string[b]) < 222:
+                        inner_list.append('+')
+                        string[b] = chr(ord(string[b] + 33))
+                        b += 1
+                    else:
+                        inner_list.append('-')
+                        string[b] = chr(ord(string[b] - 33))
+                        b += 1
+                print("inner list is: ", inner_list)
+
+                #for b in len(string):
+                 #   list[b] = chr(ord(string[b] + 33)) + '+'
+
+
+
+    #elif pkey == '@':
+    #elif pkey == '#':
+    #elif pkey == '$':
+    #elif pkey == '%':
+    #else:
 
 
 def switch_state(argument):
@@ -65,7 +107,7 @@ def switch_state(argument):
         keylist = '(' + keylist + ')'
         print(keylist)
         #print("keylist is: (" + keylist + ")")
-        getEncrypt(keylist,string):
+        getEncrypt(keylist,string)
 
 
 
